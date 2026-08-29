7559cab19a8b043d65148d8a2d0e91982c17e34b
Moi Gha <ghalleb.mohamed2@gmail.com>
2026-08-29 07:55:15 +0200

    GitHub Page Config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/RAM_Tools/',
   server: {
     port: 3000,
     host: '0.0.0.0',
